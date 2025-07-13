e7c9915ecb2f7dd48bcaa6b2cffb58eec941e495
Tarik Castro de Oliveira <105573465+TarikCO@users.noreply.github.com>
2025-07-13 17:19:36 -0300

    Update AboutSection.jsx

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -78,7 +78,7 @@ export const AboutSection = () => {
                             <a href="#contact" className="cosmic-button">
                                 Contact me
                             </a>
-                            <a href="public/TarikResume.pdf" 
+                            <a href="/TarikResume.pdf" 
                             className="px-6 py-2 rounded-full border border-primary
                                        text-primary hover:bg-primary/10 transition-colors duration-300"
                             download>
