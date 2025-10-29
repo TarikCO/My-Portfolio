06cd3a87bf4f29d43ddc6042ab2e385ba618f1fe
Tarik Castro de Oliveira <105573465+TarikCO@users.noreply.github.com>
2025-10-29 18:38:35 -0400

    Update HeroSection.jsx

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -17,7 +17,7 @@ export const HeroSection = () => {
                 <p className="text-lg md:text-xl text-muted-foreground max-w-2xl mx-auto opacity-0 animate-fade-in-delay-3">
                     I am a second year Computer Science student at the University of South Florida. 
                     I am passionate about software engineering and web development, as well as as Data-Science
-                    and using AI to automate processes.
+                    and Artificial Intelligence.
                 </p>
 
                 <div className="pt-4 opacity-0 animate-fade-in-delay-4">
